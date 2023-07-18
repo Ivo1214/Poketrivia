45899b8d2109fd3f41044ee6707261e9f354a1a8
Ivo1214 <ivo1214@live.com>
2023-07-18 15:26:41 -0300

    Final del juego
    Se añadio un final al juego y una comprobacion para que el jugador no haga trampa

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 "use strict";
 
+const maximoEncuestados = 16; // Numero maximo de pokemons a preguntar
+
 let numero = 0;  // Se utiliza para saber el nº de pokemon a adivinar
 let cantidadAcertados;  // Se utiliza para saber cuantos pokemons fueron adivinados
 let cantidadEncuestados; // Se utiliza para saber cuantas veces se encuesto al usuario
+let acerto = false; //Se usa para que el usuario sea forzado a actualizar el pokemon para continuar, de lo contrario haria trampa no cambiando el pokemon al acertar
 let pokemon;
 if (sessionStorage.getItem('cantidadEncuestados') === null) {
     sessionStorage.setItem('cantidadAcertados', 0);
@@ -12,7 +15,7 @@ if (sessionStorage.getItem('cantidadEncuestados') === null) {
 
 
 
-
+// Funcion que muestra el puntaje
 function cargarPuntaje () {
     parseInt(sessionStorage.getItem('cantidadAcertados'))>(parseInt(sessionStorage.getItem('cantidadEncuestados'))/2) ? document.getElementById("puntaje").classList.remove("puntajeBajo"):document.getElementById("puntaje").classList.add("puntajeBajo")
     
@@ -20,7 +23,9 @@ function cargarPuntaje () {
 };
 
 
+// Funcion que cambia el pokemon a adivinar
 function restart() {
+    acerto = false;
     document.querySelector("#resultText").innerHTML = "";
     document.querySelector("#imgpokemon").classList.remove("success");
     document.getElementById("input").value = "";
@@ -31,7 +36,36 @@ function restart() {
     document.getElementById("skills-description").style = "display: none;";
 }
 
+// Funcion que reinicia el juego
+function restarGame () {
+    sessionStorage.setItem('cantidadAcertados', 0);
+    sessionStorage.setItem('cantidadEncuestados',0);
+    restart();
+    cargarPuntaje();
+}
 
+// Funcion que evalua la calificacion obtenida al final de juego
+function final () {
+    const encuestados = parseInt(sessionStorage.getItem('cantidadEncuestados'));
+    if (encuestados === maximoEncuestados) {
+        const acertados = parseInt(sessionStorage.getItem('cantidadAcertados'));
+
+        if (acertados === encuestados) {
+            alert("Rango Obtenido: Master");
+        } 
+        else if (acertados > (encuestados * 0.70)){
+            alert("Rango Obtenido: Ultra");
+        } 
+        else if (acertados >= (encuestados * 0.50)){
+            alert("Rango Obtenido: Super");
+        } 
+        else {
+            alert("No has podido clasificarte");
+        }
+        alert ("Reiniciando juego...");
+        restarGame();
+    }
+}
 
 cargarPuntaje ();
 agregarEventos();
--- js/events.js
+++ js/events.js
@@ -3,36 +3,47 @@
 const send = document.getElementById('send')
 const reload = document.getElementById('reload')
 
+
 function agregarEventos() {
 
   reload.onclick = () => restart();
 
   send.onclick = () => {
     let nombreIngresado = document.getElementById("input").value;
-
-    if (nombreIngresado === "") {
-        throw new Error("No se ingreso ningun nombre.");
-    }
-    else {
-        cantidadEncuestados = sessionStorage.getItem('cantidadEncuestados');
-        cantidadEncuestados++;
-        sessionStorage.setItem('cantidadEncuestados',cantidadEncuestados);
-
-        nombreIngresado.toLowerCase();
-        if (nombreIngresado === pokemon.name.toLowerCase()){
-            document.getElementById("resultText").innerText = "Es correcto!";
-            document.getElementById("imgpokemon").classList.add("success");
-
-            cantidadAcertados = sessionStorage.getItem('cantidadAcertados');
-            cantidadAcertados++;
-            sessionStorage.setItem('cantidadAcertados',cantidadAcertados);
-            buscarPokemon ();
-            document.getElementById("skills-description").style = "";
+    if (acerto === false) { //Si el jugador adivino el pokemon anterior entonces no podra avanzar en el juego hasta que use el boton restart
+        if (nombreIngresado === "") {
+            throw new Error("No se ingreso ningun nombre.");
         }
         else {
-            document.getElementById("resultText").innerText = "Es incorrecto!";
+            cantidadEncuestados = sessionStorage.getItem('cantidadEncuestados');
+            cantidadEncuestados++;
+            sessionStorage.setItem('cantidadEncuestados',cantidadEncuestados);
+    
+            nombreIngresado.toLowerCase();
+            if (nombreIngresado === pokemon.name.toLowerCase()){
+                acerto = true;
+
+                document.getElementById("resultText").innerText = "Es correcto!";
+                document.getElementById("imgpokemon").classList.add("success");
+    
+                cantidadAcertados = sessionStorage.getItem('cantidadAcertados');
+                cantidadAcertados++;
+                sessionStorage.setItem('cantidadAcertados',cantidadAcertados);
+    
+                buscarPokemon ();
+                document.getElementById("skills-description").style = "";
+    
+            }
+            else {
+                document.getElementById("resultText").innerText = "Es incorrecto!";
+            }
+            cargarPuntaje ();
         }
-        cargarPuntaje ();
+        final();
     }
+    else {
+        alert("Debes actualizar el pokemon a adivinar...");
+    }
+    
 }
 }
